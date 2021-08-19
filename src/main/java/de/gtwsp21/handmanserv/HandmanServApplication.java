4a16521ac70a4141d414ab31eea692a6f7daff9f
gwuttke <georg1wuttke@gmail.com>
2021-08-19 19:02:37 +0200

    init project HandmanServ
    initialisiere das Projekt HandmanServ mit Gradle

---
+++ src/main/java/de/gtwsp21/handmanserv/HandmanServApplication.java
@@ -0,0 +1,13 @@
+package de.gtwsp21.handmanserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandmanServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandmanServApplication.class, args);
+	}
+
+}
