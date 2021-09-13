bcf7bd36417d59aab58e0a92deddb068faf164d2
gwuttke <georg1wuttke@gmail.com>
2021-09-13 12:04:30 +0200

    Stammdaten hinzugefügt

---
+++ src/main/resources/db/migration/V2021091220_dml_insert_gebiete.sql
@@ -0,0 +1,11 @@
+INSERT INTO gebiete(bereich,name) VALUES 
+('0','Sachsen,Thüringen,Sachsen-Anhalt,Brandenburg'),
+('1','Mecklenburg-Vorpommern,Brandenburg,Berlin,Niedersachsen,Sachsen-Anhalt'),
+('2','Schleswig-Holstein,Niedersachsen,Mecklenburg-Vorpommern,Bremen,Hamburg,Sachsen-Anhalt'),
+('3','Niedersachsen,Hessen,Sachsen-Anhalt,Thüringen,Nordrhein-Westfalen'),
+('4','Nordrhein-Westfalen,Niedersachsen'),
+('5','Rheinland-Pfalz,Nordrhein-Westfalen,Hessen'),
+('6','Rheinland-Pfalz,Hessen,Bayern,Saarland,Baden-Württemberg'),
+('7','Baden-Württemberg,Rheinland-Pfalz,Bayern'),
+('8','Bayern,Baden-Württemberg'),
+('9','Bayern,Thüringen,Baden-Württemberg');
